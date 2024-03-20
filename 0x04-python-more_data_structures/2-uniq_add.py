e53bbc6c05fad1db13f892af076a07ed4b5335ec
Ella-Ensermu <142889164+Ella-Ensermu@users.noreply.github.com>
2024-03-20 23:02:33 +0300

    Update 2-uniq_add.py

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-uniq_add = __import__('2').uniq_add
-
-my_list = [1, 2, 3, 1, 4, 2, 5]
-result = uniq_add(my_list)
-print("Result: {:d}".format(result))
+def uniq_add(my_list=[]):
+    res = 0
+    for i in my_list:
+        res = sum(set(my_list))
+        return res
