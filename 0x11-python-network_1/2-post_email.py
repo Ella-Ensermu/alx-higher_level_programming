c8efd8ee23ae82914a84800a059455f53dbb7db4
Ella-Ensermu <142889164+Ella-Ensermu@users.noreply.github.com>
2024-07-07 22:48:36 +0300

    Update 2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -4,16 +4,18 @@ Usage: ./2-post_email.py <URL> <email>
   - Displays the body of the response.
 """
 
-
 import sys
 import urllib.parse
 import urllib.request
 
-
 if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print("Usage: ./2-post_email.py <URL> <email>")
+        sys.exit(1)
+
     url = sys.argv[1]
-    value = {"email": sys.argv[2]}
-    data = urllib.parse.urlencode(value).encode("ascii")
+    email = sys.argv[2]
+    data = urllib.parse.urlencode({"email": email}).encode("ascii")
 
     request = urllib.request.Request(url, data)
     with urllib.request.urlopen(request) as response:
